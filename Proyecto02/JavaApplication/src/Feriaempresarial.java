246584bd32d0145871ef2fd1d1246c979579ddd1
Gonzalo Daniel <gonzalodaniel@google.com>
2025-02-17 18:49:44 -0500

    Agregando stand service

--- Proyecto02/JavaApplication/src/Feriaempresarial.java
+++ Proyecto02/JavaApplication/src/Feriaempresarial.java
@@ -1,9 +1,11 @@
 
- import services.EmpresaService;
- import enums.Sectores;
- import models.Empresa;
- 
- import java.util.List;
+import services.EmpresaService;
+import services.StandService;
+import enums.Sectores;
+import models.Empresa;
+import models.Stand;
+
+import java.util.List;
  
  /**
   *
@@ -12,10 +14,11 @@
  public class Feriaempresarial {
   public static void main(String[] args) {
     EmpresaService empresaService = new EmpresaService();
+    StandService standService = new StandService();
     
     // Registrar empresas
     System.out.println("\nRegistrando 2 empresas");
-    empresaService.registrarEmpresa("Tech Corp", Sectores.TECNOLOGIA, "test@techcorp.com");
+    Empresa empresa1 = empresaService.registrarEmpresa("Tech Corp", Sectores.TECNOLOGIA, "test@techcorp.com");
     empresaService.registrarEmpresa("Umbrella", Sectores.SALUD, "t-virus@umbrella.com");
     
     
@@ -34,6 +37,22 @@
     empresas = empresaService.listarEmpresas();
     for (Empresa empresa : empresas) {
       System.out.println("- " + empresa.getNombre() + " | Sector: " + empresa.getSector().getDescripcion() + " | Email: " + empresa.getEmail());
+    }
+
+
+    // Listar stands disponibles
+    System.out.println("\nStands disponibles:");
+    for (Stand stand : standService.listarStandsDisponibles()) {
+        System.out.println("- Stand " + stand.getNumero() + " | Ubicación: " + stand.getUbicacion().getDescripcion() + " | Tamaño: " + stand.getTamano());
+    }
+
+    // Asignar un stand a una empresa
+    standService.asignarStand(1, "Tech Corp");
+
+    // Listar stands después de asignación
+    System.out.println("\nStands después de asignación:");
+    for (Stand stand : standService.listarStandsDisponibles()) {
+        System.out.println("- Stand " + stand.getNumero() + " | Ubicación: " + stand.getUbicacion().getDescripcion() + " | Tamaño: " + stand.getTamano());
     }
   }
  }
