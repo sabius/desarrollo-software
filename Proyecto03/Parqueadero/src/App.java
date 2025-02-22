81a17b35e296527d7f54e4c952ae6f1d9f410a69
Gonzalo Daniel <gonzalodaniel@google.com>
2025-02-21 22:27:50 -0500

    Limpiando proyecto 3

--- Proyecto03/Parqueadero/src/App.java
+++ Proyecto03/Parqueadero/src/App.java
@@ -1,17 +1,25 @@
 import models.Vehiculo;
-import models.Camion;
+// import models.Camion;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Camion frordcCamion = new Vehiculo("ABC123", "kia", "cmion", "10am" );
-        Vehiculo honda = new Vehiculo( "eexx75", "honda", "k42", "1pm");
+        Vehiculo picanto = new Vehiculo(
+          "ABC 123",
+          "Kia",
+          "Picanto",
+          java.time.LocalDateTime.now()
+        );
 
-
-
+        Vehiculo honda = new Vehiculo(
+          "EEX 475",
+          "Honda",
+          "k42",
+          java.time.LocalDateTime.now()
+        );
 
         System.out.println("la placa de picanto es: " + picanto.getPlaca());
         
-        picanto.setPlaca("ssZ987");   
+        picanto.setPlaca("ZXC 987");
         System.out.println("la placa de picanto es: " +picanto.getPlaca());
             
         System.out.println("la marca de honda es: " + honda.getMarca());
--- Proyecto03/Parqueadero/src/models/Vehiculo.java
+++ Proyecto03/Parqueadero/src/models/Vehiculo.java
@@ -1,37 +1,51 @@
 package models;
 
+import java.time.LocalDateTime;
 
 public class Vehiculo {
     private String placa;
     private String marca;
     private String modelo;
-    private java.time.LocalDate horaEntrada;
+    private java.time.LocalDateTime horaEntrada;
 
     // Constructor
-    public Vehiculo(String placa, String marca, String modelo, java.time.LocalDate horaEntrada) {
-        this.placa = placa;
-        this.marca = marca;
-        this.modelo = modelo;
-        this.horaEntrada = horaEntrada;
+    public Vehiculo(String placa, String marca, String modelo, LocalDateTime horaEntrada) {
+      this.placa = placa;
+      this.marca = marca;
+      this.modelo = modelo;
+      this.horaEntrada = horaEntrada;
     }
 
     // Getters and setters
     public String getPlaca() {
-        return placa;} public void setPlaca(String placa) {
-        this.placa = placa;}
+      return placa;
+    }
+    
+    public void setPlaca(String placa) {
+      this.placa = placa;
+    }
 
     public String getMarca() {
-        return marca;}  public void setMarca(String marca) {
-        this.marca = marca;}
+      return marca;
+    }
+    
+    public void setMarca(String marca) {
+      this.marca = marca;
+    }
 
     public String getModelo() {
-        return modelo;}   public void setModelo(String modelo) {
-        this.modelo = modelo; }
-
-    public java.time.LocalDate getHoraEntrada() {
-        return horaEntrada;} public void setHoraEntrada(java.time.LocalDate horaEntrada) {
-        this.horaEntrada = horaEntrada;}
+      return modelo;
+    }
     
-    
+    public void setModelo(String modelo) {
+        this.modelo = modelo; 
+    }
 
+    public java.time.LocalDateTime getHoraEntrada() {
+        return horaEntrada;
+    }
+    
+    public void setHoraEntrada(java.time.LocalDateTime horaEntrada) {
+      this.horaEntrada = horaEntrada;
+    }
 }
