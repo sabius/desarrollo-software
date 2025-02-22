bfe144ab99b5efbc8051e20b12ace066cd91fcde
IngBrayanPro <ingbrayanprosistem@gmail.com>
2025-02-21 23:08:01 -0500

    se arreglo un problema de ortografia

--- Proyecto03/Parqueadero/src/App.java
+++ Proyecto03/Parqueadero/src/App.java
@@ -1,3 +1,4 @@
+import models.Camion;
 import models.Vehiculo;
 // import models.Camion;
 
@@ -7,21 +8,31 @@ public class App {
           "ABC 123",
           "Kia",
           "Picanto",
-          java.time.LocalDateTime.now()
+          java.time.LocalDateTime.now(),
+          "diesel",
+          3000  
         );
 
         Vehiculo honda = new Vehiculo(
           "EEX 475",
           "Honda",
           "k42",
-          java.time.LocalDateTime.now()
+          java.time.LocalDateTime.now(),
+          "Gasolina",
+          5000
         );
 
-        System.out.println("la placa de picanto es: " + picanto.getPlaca());
-        
-        picanto.setPlaca("ZXC 987");
-        System.out.println("la placa de picanto es: " +picanto.getPlaca());
-            
-        System.out.println("la marca de honda es: " + honda.getMarca());
+        Camion camion = new Camion(
+          "EEX 475",
+          "frod",
+          "k42",
+          java.time.LocalDate.now(),
+          "Gasolina",
+          5000
+        );
+
+        System.out.println("la marca de veiculo es: " + picanto.getMarca());
+        System.out.println("la marca de veiculo es: " + honda.getMarca());
+        System.out.println("la marca de camion es: " + camion.getMarca());
     }
 }
--- Proyecto03/Parqueadero/src/models/Vehiculo.java
+++ Proyecto03/Parqueadero/src/models/Vehiculo.java
@@ -7,45 +7,44 @@ public class Vehiculo {
     private String marca;
     private String modelo;
     private java.time.LocalDateTime horaEntrada;
+    private String tipoCombustible;
+    private int valorTarifa;
 
     // Constructor
-    public Vehiculo(String placa, String marca, String modelo, LocalDateTime horaEntrada) {
+    public Vehiculo(String placa, String marca, String modelo, LocalDateTime horaEntrada, String tipoCombustible, int valorTarifa) {
       this.placa = placa;
       this.marca = marca;
       this.modelo = modelo;
       this.horaEntrada = horaEntrada;
+      this.tipoCombustible = tipoCombustible;
+      this.valorTarifa = valorTarifa;
     }
 
     // Getters and setters
     public String getPlaca() {
       return placa;
-    }
-    
-    public void setPlaca(String placa) {
-      this.placa = placa;
-    }
+    }public void setPlaca(String placa) {
+      this.placa = placa;}
 
     public String getMarca() {
       return marca;
-    }
-    
-    public void setMarca(String marca) {
-      this.marca = marca;
-    }
+    }public void setMarca(String marca) {
+      this.marca = marca;}
 
     public String getModelo() {
       return modelo;
-    }
-    
-    public void setModelo(String modelo) {
-        this.modelo = modelo; 
-    }
-
+    }public void setModelo(String modelo) {
+        this.modelo = modelo;}
+ 
     public java.time.LocalDateTime getHoraEntrada() {
-        return horaEntrada;
-    }
-    
-    public void setHoraEntrada(java.time.LocalDateTime horaEntrada) {
-      this.horaEntrada = horaEntrada;
-    }
+        return horaEntrada;}public void setHoraEntrada(java.time.LocalDateTime horaEntrada) {
+      this.horaEntrada = horaEntrada;}
+
+      public String  getTipoConbustible() {
+        return tipoCombustible;} public void setTipoCombustible(String  tipoCombustible) {
+        this.tipoCombustible = tipoCombustible;}
+
+      public int  getValorTarifa() {
+            return valorTarifa;} public void setValorTarifa(int valorTarifa) {
+            this.valorTarifa = valorTarifa;}
 }
